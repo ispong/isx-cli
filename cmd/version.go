5fa0c329479bf077561dd9d5a8b522e663c6ac7d
ispong <ispong@outlook.com>
2023-12-15 16:45:12 +0800

    :sparkles: 补齐剩余命令

--- cmd/version.go
+++ cmd/version.go
@@ -1,19 +1,34 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(versionCmd)
-}
-
+// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "v1.0.0",
-	Long:  `All software has versions. This is Hugo's`,
+	Short: "查看当前版本号，举例：isx version",
+	Long:  `查看当前版本号，举例：isx version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("当前最新版本v1.0.0")
+		fmt.Println("version called")
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
